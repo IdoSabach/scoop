ba29c3894c3d50a38d7c1a95856b5365d38a0f9e
Sam <oxedom@gmail.com>
2024-06-18 21:49:16 +0300

    fix: nodemon, screenshots - main

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-const express = require('express');
-const { chromium } = require('playwright');
-
+const express = require("express");
+const { chromium } = require("playwright");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require("path");
+const fs = require("fs");
+if (!fs.existsSync("screenshots")) fs.mkdirSync("screenshots");
 
 // Initialize a browser pool
 const browserPool = {
@@ -10,7 +12,7 @@ const browserPool = {
   maxInstances: 5,
   async init() {
     for (let i = 0; i < this.maxInstances; i++) {
-      const browser = await chromium.launch();
+      const browser = await chromium.launch({ headless: true });
       this.browsers.push(browser);
     }
   },
@@ -28,34 +30,38 @@ const browserPool = {
 
 // Initialize the browser pool before the server starts
 browserPool.init().then(() => {
-  console.log('Browser pool initialized');
+  console.log("Browser pool initialized");
 });
 
-app.get('/check-h1', async (req, res) => {
+app.get("/screenshot", async (req, res) => {
   const { url } = req.query;
 
   if (!url) {
-    return res.status(400).json({ error: 'URL query parameter is required' });
+    return res.status(400).json({ error: "URL query parameter is required" });
   }
 
   let browser;
   try {
     browser = await browserPool.getBrowser();
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
 
-    const h1Text = await page.$eval('h1', el => el.textContent);
+    await page.pause();
+    await page.screenshot({
+      path: path.join(process.cwd(), "screenshots", `${Date.now()}.png`),
+      fullPage: true,
+    });
 
     await page.close();
     browserPool.releaseBrowser(browser);
 
-    res.json({ h1Text });
+    res.json({ message: `Screenshot taken of ${url} ` });
   } catch (error) {
     if (browser) {
       browserPool.releaseBrowser(browser);
     }
     console.error(error);
-    res.status(500).json({ error: 'Failed to extract h1 text' });
+    res.status(500).json({ error: "Failed to extract h1 text" });
   }
 });
 
